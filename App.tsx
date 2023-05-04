230b444ec987f4fefe9051227174cfa32803a806
Bela Bohlender <bbohlender@coconut-capital.com>
2023-05-04 12:25:17 +0200

    image working with koestlich@v0.0.10

--- App.tsx
+++ App.tsx
@@ -1,34 +1,32 @@
 import React, { Suspense, useRef } from "react";
 import { StyleSheet, View } from "react-native";
-import { Image as RNImage } from 'react-native';
+import { Image as RNImage } from "react-native";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { RootContainer, Text, SVG, Box, Image, Container } from "@coconut-xr/koestlich";
-import loadYogaSync from "yoga-wasm-web"
-import { PerspectiveCamera } from "three";
-import image from "./assets/icon.png"
-import useControls from 'r3f-native-orbitcontrols';
+import {
+  RootContainer,
+  Text,
+  SVG,
+  Box,
+  Image,
+  Container,
+} from "@coconut-xr/koestlich";
+import loadYogaSync from "yoga-wasm-web";
+import { Mesh, PerspectiveCamera } from "three";
+//@ts-ignore
+import image from "./assets/icon.png";
 import { useTexture } from "@react-three/drei";
 
-const imageUrl = "./assets/icon.png";
-console.log(`imP: ${image}, source: ${JSON.stringify(RNImage.resolveAssetSource(image))}, uri: ${imageUrl}`);
-
 async function loadYoga() {
-  return (loadYogaSync as any)()
+  return (loadYogaSync as any)();
 }
 
-function
-DaBox(props)
-{
-  const mesh = useRef(null);
-  const texture = useTexture(image)
-  useFrame((state, delta) => (mesh.current.rotation.y += 0.007));
+function DaBox(props: any) {
+  const mesh = useRef<Mesh>(null);
+  const texture = useTexture(image);
+  useFrame((state, delta) => (mesh.current!.rotation.y += 0.007));
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={1}
-    >
+    <mesh {...props} ref={mesh} scale={1}>
       <boxGeometry args={[0.5, 0.5, 0.04]} />
       <meshBasicMaterial map={texture} />
     </mesh>
@@ -36,19 +34,27 @@ DaBox(props)
 }
 
 export default function App() {
-  const [OrbitControls, events] = useControls();
-
   return (
-    <View style={styles.container} {...events} >
-      <RNImage source={image} style={{ width: 128, height: 128 }} />
-      <Canvas gl={{ localClippingEnabled: true }}>
+    <View style={styles.container}>
+      <Canvas
+        onCreated={(state) => {
+          const _gl = state.gl.getContext();
+          const pixelStorei = _gl.pixelStorei.bind(_gl);
+          _gl.pixelStorei = function (...args) {
+            const [parameter] = args;
+            switch (parameter) {
+              case _gl.UNPACK_FLIP_Y_WEBGL:
+                return pixelStorei(...args);
+            }
+          };
+        }}
+        gl={{ localClippingEnabled: true }}
+      >
         <pointLight intensity={0.5} position={[0, 0, -1]} />
         <ambientLight intensity={0.5} />
-        <OrbitControls />
         <Suspense>
-          <DaBox />
+          <UI />
         </Suspense>
-        <UI />
       </Canvas>
     </View>
   );
@@ -56,14 +62,26 @@ export default function App() {
 
 function UI() {
   const ratio = useThree((s) => s.size.width / s.size.height);
-  const camera = useThree(s => s.camera as PerspectiveCamera)
-  camera.position.set(ratio / 2, -0.5, 0.5)
-  camera.fov = 90
-  camera.updateProjectionMatrix()
+  const camera = useThree((s) => s.camera as PerspectiveCamera);
+  camera.position.set(ratio / 2, -0.5, 0.5);
+  camera.fov = 90;
+  camera.updateProjectionMatrix();
   return (
     <Suspense>
-      <RootContainer loadYoga={loadYoga} width={ratio} height={1} gapColumn={0.1} gapRow={0.1} padding={0.1} backgroundColor="red">
-        <Container backgroundColor="blue" flexGrow={1}></Container>
+      <RootContainer
+        loadYoga={loadYoga}
+        width={ratio}
+        height={1}
+        gapColumn={0.1}
+        gapRow={0.1}
+        padding={0.1}
+        backgroundColor="red"
+        alignItems="center"
+      >
+        <Container width="100%" backgroundColor="blue" flexGrow={1}></Container>
+        <Suspense>
+          <Image url={image} height={0.2}></Image>
+        </Suspense>
         {/*
           * Move any following lines outside the comment block to test.
         <Image url={image} flexGrow={1}></Image>
---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+1. install dependecies `npm i --legacy-peer-deps`
+2. edit `node_modules/yoga-wasm-web/package.json` and add the line `"main": "./dist/asm.js",`
+3. run `npm run start` and scan the qr code with the expo go app
